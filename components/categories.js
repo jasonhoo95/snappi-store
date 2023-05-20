2b15aa088149ed84696382c09aca97c2986524ab
hoo chong yi <hoochongyi95@gmail.com>
2023-05-20 15:39:47 +0800

    done fixing padding issue

--- components/categories.js
+++ components/categories.js
@@ -2,170 +2,170 @@ export default function Category() {
 	return (
 		<div className="grid main-category my-3 text-black">
 			<div className="grey-card">
-				<div className="w-[80%] h-[70px] mb-3">
+				<div className="w-[80%] h-[70px]">
 					<img
 						className="w-full h-full object-contain"
 						src="/icon-automotive-dark-pink.png"
 					/>
 				</div>
 
-				<div className="w-full ">Automotive</div>
+				<div className="w-full mt-3 h-[30px]">Automotive</div>
 			</div>
 			<div className="grey-card">
-				<div className="w-[80%] h-[70px] mb-3">
+				<div className="w-[80%] h-[70px] ">
 					<img
 						className="w-full h-full object-contain"
 						src="/icon-baby.png"
 					/>
 				</div>
 
-				<div className="w-full">Baby & toddler</div>
+				<div className="w-full mt-3 h-[30px]">Baby & toddler</div>
 			</div>
 			<div className="grey-card">
-				<div className="w-[80%] h-[70px] mb-3">
+				<div className="w-[80%] h-[70px] ">
 					<img
 						className="w-full h-full object-contain"
 						src="/icon-beauty-health-pink.png"
 					/>
 				</div>
 
-				<div className="w-full">Beauty & Health</div>
+				<div className="w-full mt-3 h-[30px]">Beauty & Health</div>
 			</div>
 			<div className="grey-card">
-				<div className="w-[80%] h-[70px] mb-3">
+				<div className="w-[80%] h-[70px] ">
 					<img
 						className="w-full h-full object-contain"
 						src="/icon-camping-outdoor-pink.png"
 					/>
 				</div>
 
-				<div className="w-full">Camping & Outdoor</div>
+				<div className="w-full mt-3 h-[30px]">Camping & Outdoor</div>
 			</div>
 			<div className="grey-card">
-				<div className="w-[80%] h-[70px] mb-3">
+				<div className="w-[80%] h-[70px] ">
 					<img
 						className="w-full h-full object-contain"
 						src="/icon_dairy_pink.png"
 					/>
 				</div>
 
-				<div className="w-full">Dairy & Alternative</div>
+				<div className="w-full mt-3 h-[30px]">Dairy & Alternative</div>
 			</div>
 			<div className="grey-card">
-				<div className="w-[80%] h-[70px] mb-3">
+				<div className="w-[80%] h-[70px] ">
 					<img
 						className="w-full h-full object-contain"
 						src="/icon-drinks.png"
 					/>
 				</div>
 
-				<div className="w-full">Drinks</div>
+				<div className="w-full mt-3 h-[30px]">Drinks</div>
 			</div>
 			<div className="grey-card">
-				<div className="w-[80%] h-[70px] mb-3">
+				<div className="w-[80%] h-[70px] ">
 					<img
 						className="w-full h-full object-contain"
 						src="/icon-electronics-pink.png"
 					/>
 				</div>
 
-				<div className="w-full">Electronics</div>
+				<div className="w-full mt-3 h-[30px]">Electronics</div>
 			</div>
 			<div className="grey-card">
-				<div className="w-[80%] h-[70px] mb-3">
+				<div className="w-[80%] h-[70px] ">
 					<img
 						className="w-full h-full object-contain"
 						src="/icon-fashion-pink.png"
 					/>
 				</div>
 
-				<div className="w-full">Fashion</div>
+				<div className="w-full mt-3 h-[30px]">Fashion</div>
 			</div>
 			<div className="grey-card">
-				<div className="w-[80%] h-[70px] mb-3">
+				<div className="w-[80%] h-[70px] ">
 					<img
 						className="w-full h-full object-contain"
 						src="/icon-foodcupboard.png"
 					/>
 				</div>
 
-				<div className="w-full">Food Cupboard</div>
+				<div className="w-full mt-3 h-[30px]">Food Cupboard</div>
 			</div>
 			<div className="grey-card">
-				<div className="w-[80%] h-[70px] mb-3">
+				<div className="w-[80%] h-[70px] ">
 					<img
 						className="w-full h-full object-contain"
 						src="/icon-garden-outdoor-pink.png"
 					/>
 				</div>
 
-				<div className="w-full">Garden & Outdoor</div>
+				<div className="w-full mt-3 h-[30px]">Garden & Outdoor</div>
 			</div>
 
 			<div className="grey-card">
-				<div className="w-[80%] h-[70px] mb-3">
+				<div className="w-[80%] h-[70px] ">
 					<img
 						className="w-full h-full object-contain"
 						src="/icon-home-accessories-pink.png"
 					/>
 				</div>
 
-				<div className="w-full">Home & Accessories</div>
+				<div className="w-full mt-3 h-[30px]">Home & Accessories</div>
 			</div>
 
 			<div className="grey-card">
-				<div className="w-[80%] h-[70px] mb-3">
+				<div className="w-[80%] h-[70px] ">
 					<img
 						className="w-full h-full object-contain"
 						src="/icon-home-improvement-pink.png"
 					/>
 				</div>
 
-				<div className="w-full">Home improvement</div>
+				<div className="w-full mt-3 h-[30px]">Home improvement</div>
 			</div>
 
 			<div className="grey-card">
-				<div className="w-[80%] h-[70px] mb-3">
+				<div className="w-[80%] h-[70px] ">
 					<img
 						className="w-full h-full object-contain"
 						src="/icon-household-pink.png"
 					/>
 				</div>
 
-				<div>Household</div>
+				<div className="w-full mt-3 h-[30px]">Household</div>
 			</div>
 
 			<div className="grey-card">
-				<div className="w-[80%] h-[70px] mb-3">
+				<div className="w-[80%] h-[70px] ">
 					<img
 						className="w-full h-full object-contain"
 						src="/icon-musicbooksdvd.png"
 					/>
 				</div>
 
-				<div className="w-full">Music, Book & Dvds</div>
+				<div className="w-full mt-3 h-[30px]">Music, Book & Dvds</div>
 			</div>
 
 			<div className="grey-card">
-				<div className="w-[80%] h-[70px] mb-3">
+				<div className="w-[80%] h-[70px] ">
 					<img
 						className="w-full h-full object-contain"
 						src="/icon-office-stationary-pink.png"
 					/>
 				</div>
 
-				<div className="w-full">Office & Stationary</div>
+				<div className="w-full mt-3 h-[30px]">Office & Stationary</div>
 			</div>
 
 			<div className="grey-card">
-				<div className="w-[80%] h-[70px] mb-3">
+				<div className="w-[80%] h-[70px] ">
 					<img
 						className="w-full h-full object-contain"
 						src="/icon-pets-pink.png"
 					/>
 				</div>
 
-				<div className="w-full">Pets</div>
+				<div className="w-full mt-3 h-[30px]">Pets</div>
 			</div>
 		</div>
 	);
